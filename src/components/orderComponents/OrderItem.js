889ff7bed49f697d03fd4f40528f3c029e288a7f
ariegold <ariegoldkin88@gmail.com>
2020-08-26 03:10:52 +0300

    working on order item

---
+++ src/components/orderComponents/OrderItem.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import Card from "../common/UIElements/Card";
+
+const OrderItem = () => {
+  return <Card>ORDER ITEM COMPONENT</Card>;
+};
+
+export default OrderItem;
